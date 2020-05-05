e9555ef65c13e47b5d2cd776ec5e6d6a92c86364
Jaime Ribeiro <jaimeribeiro99@gmail.com>
2020-05-05 01:01:21 -0300

    :sparkles: Adding Navbar and buttons(sand & dot)

---
+++ src/components/header/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar color="deep-purple accent-4" dense dark>
+    <!-- dense retira os paddings que vem como padrão -->
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <!-- icone nav-bar padrão sanduíche -->
+    <v-toolbar-title>He4rt Developers</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <!-- sem o v-spacer o menu 3dots fica colado no title he4rtdev -->
+    <v-menu left bottom>
+      <!-- left bottom são pra controlar posição dos itens dentro do menu de três pontos -->
+      <template v-slot:activator="{on}">
+        <v-btn icon v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+          <!-- adicionando 3 pontos lado direito -->
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="({titulo,href},index) in itemLista" :key="index" @click="()=>{}">
+          <v-list-item-title>{{titulo}}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      itemLista: [
+        { titulo: "Registrar", href: "" },
+        { titulo: "Entrar", href: "" }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
